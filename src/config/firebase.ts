d6bd9566fae6ba3d37ae9469cdfdbd2682e6c945
VitorFigueiredoDev <vitorfigueiredo2005dev@gmail.com>
2025-05-23 17:30:49 -0300

    Fix: Atualiza configuração do Firebase para carregar API Key de variável de ambiente

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfO6cZCnZRF6fIwW1gBQi-w7gtSx3x_PM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
